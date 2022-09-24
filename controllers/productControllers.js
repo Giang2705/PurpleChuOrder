d830099a44d6017fc5ae0dfdd51d33d439cb6669
Giang2705 <s3914108@rmit.edu.vn>
2022-09-24 14:51:22 +0700

    product filtering | sorting | paginating - be

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,11 +1,60 @@
 const Products = require("../models/productModel")
 
+// Filter, sorting and paginating
+class APIfeatures {
+    constructor(query, queryString){
+        this.query = query;
+        this.queryString = queryString;
+    }
+    filtering(){
+        const queryObject = {...this.queryString} //queryString = req.query
+
+        const excludedFields = ['page', 'sort', 'limit']
+        excludedFields.forEach(el => delete(queryObject[el]))
+
+        let queryStr = JSON.stringify(queryObject)
+
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
+
+        this.query.find(JSON.parse(queryStr))
+
+        return this;
+    }
+    sorting(){
+        if(this.queryString.sort){
+            const sortBy = this.queryString.sort.split(',').join(' ')
+            this.query= this.query.sort(sortBy)
+        } else {
+            this.query = this.query.sort('-createdAt')
+        }
+
+        return this;
+    }
+    paginating(){
+        const page = this.queryString.page * 1 || 1 
+        const limit = this.queryString.limit * 1 || 9
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit)
+
+        return this;
+    }
+}
+
+
+// Product CRUD
 const productControllers = {
     getProducts: async(req, res) => {
         try {
-            const products = await Products.find()
+            console.log(req.query)
+            const features = new APIfeatures(Products.find(), req.query).filtering().sorting().paginating()
+            
+            const products = await features.query
 
-            res.json(products)
+            res.json({
+                status: "success",
+                result: products.length,
+                products: products
+            })
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
