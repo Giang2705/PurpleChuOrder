853c0a02cff2fe2767071240b18625608743437c
Giang2705 <s3914108@rmit.edu.vn>
2022-11-10 10:50:22 +0700

    make it better

--- client/src/components/productItem/ProductItem.jsx
+++ client/src/components/productItem/ProductItem.jsx
@@ -10,11 +10,29 @@ const ProductItem = ({ product }) => {
       <div className="product_box">
         <h2>{product.name}</h2>
         {
-          <h5>Version: {product.version[0].ver !== "" ? `${product.version[0].ver}` : ""} </h5> 
+          <h5>
+            Version:{" "}
+            {product.version.length > 1 && product.version[0].ver !== ""
+              ? product.version.map((item) => {
+                  return item.ver + ", ";
+                })
+              : product.version[0].ver}{" "}
+          </h5>
         }
         <div className="row">
           <span>
-            {product.version[0].price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")} VND
+            {product.version.length > 1 && product.version[0].ver !== ""
+              ? product.version[0].price
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ".") +
+                " - " +
+                product.version[product.version.length-1].price
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+              : product.version[0].price
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}{" "}
+            VND
           </span>
 
           <BtnRender product={product} />
